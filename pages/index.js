185fb3afe2d21ac838b425a0c2f0be45645905b6
Tms.dev <105759146+425tomake@users.noreply.github.com>
2026-01-14 02:57:49 +0000

    Insercao da tag link

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 function Home(){
     return <h1> ⚠️Se vc ta vendo isso é pq os bbs vão dominar o mundo!
-        Descubra como: https://youtube.com/shorts/V8DEMvPqrgw?si=SNfC-X6q6_UNsSvI
-    </h1>    
+        Descubra como: 
+        <link> https://youtube.com/shorts/V8DEMvPqrgw?si=SNfC-X6q6_UNsSvI
+       </link>
+        </h1>
 
 
 }
