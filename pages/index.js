e0449dd00daf76ad72647ca61a729bef014a5b00
Tms.dev <105759146+425tomake@users.noreply.github.com>
2026-01-14 02:53:14 +0000

    Adição do link do video fofo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home(){
-    return <h1>Tab News do Curso.Dev</h1>    
+    return <h1> ⚠️Se vc ta vendo isso é pq os bbs vão dominar o mundo!
+        Descubra como: https://youtube.com/shorts/V8DEMvPqrgw?si=SNfC-X6q6_UNsSvI
+    </h1>    
 
 
 }
