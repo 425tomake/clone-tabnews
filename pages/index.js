1ea3c3482dd4e9013a5f24f9e2881b414698937f
Tms.dev <105759146+425tomake@users.noreply.github.com>
2026-03-18 04:29:29 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
-function Home(){
-    return <h1> ⚠️Se vc ta vendo isso é pq os bbs vão dominar o mundo!
-        Descubra como: 
-        <link> https://youtube.com/shorts/V8DEMvPqrgw?si=SNfC-X6q6_UNsSvI
-       </link>
-        </h1>
-
-
+function Home() {
+  return (
+    <h1>
+      {" "}
+      ⚠️Se vc ta vendo isso é pq os bbs vão dominar o mundo! Descubra como:
+      <a
+        href="https://youtube.com/shorts/V8DEMvPqrgw?si=SNfC-X6q6_UNsSvI"
+        target="_blank"
+      >
+        Clique aqui
+      </a>
+    </h1>
+  );
 }
 
 export default Home;
-
